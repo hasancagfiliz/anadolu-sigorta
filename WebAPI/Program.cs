fd9f2e95b0b0192331c29fc82c32d98406463623
hasancagfiliz <cagfiliz@gmail.com>
2024-08-27 15:40:17 +0300

    visual studio

---
+++ WebAPI/Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+ // Routing
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
