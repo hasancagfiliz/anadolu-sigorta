d7d898712813b9104adf02273cf94c961afff841
hasancagfiliz <cagfiliz@gmail.com>
2024-08-27 16:05:23 +0300

    shirts api demo

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -6,9 +6,36 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-app.UseHttpsRedirection();
 
- // Routing
+
+// Routing
+
+// "shirts"
+
+app.MapGet("/shirts", () =>
+{
+    return "Reading all the shirts";
+});
+
+app.MapGet("shirts/{id}", (int id) =>
+{
+    return $"Reading shirt with ID: {id}";
+});
+
+app.MapPost("shirts/{id}", (int id) =>
+{
+    return $"Creating a shirt with ID: {id}";
+});
+
+app.MapPut("/shirts/{id}", (int id) =>
+{
+    return $"Updating shirt with ID: {id}";
+});
+
+app.MapDelete("/shirts/{id}", (int id) =>
+{
+    return $"Deleting shirt with ID: {id}";
+});
 
 app.Run();
 
--- WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e26558407d39ad066c2237a153e34d5221ba6e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd9f2e95b0b0192331c29fc82c32d98406463623")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
